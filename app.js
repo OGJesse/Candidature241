bb269109a7929fdcb4a5098abeb55b324300ff3a
Jesse <88139108+OGJesse@users.noreply.github.com>
2022-02-08 19:11:05 +0100

    update

---
+++ app.js
@@ -0,0 +1,18 @@
+
+// Modal
+
+const btnmodal = document.querySelector('.btn');
+const modalview = document.querySelector('.modal-container');
+const close = document.querySelector('.close-modal')
+
+
+btnmodal.addEventListener('click',() => {
+
+    modalview.classList.toggle('active')
+})
+
+close.addEventListener('click',() => {
+
+    modalview.classList.remove('active')
+})
+
